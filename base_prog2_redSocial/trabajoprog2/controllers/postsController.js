c24a696426887371a5f575929b35f4d6f028eace
priscillaarslan <parslan@udesa.edu.ar>
2022-09-18 17:28:20 -0300

    agregue

--- base_prog2_redSocial/trabajoprog2/controllers/postsController.js
+++ base_prog2_redSocial/trabajoprog2/controllers/postsController.js
@@ -9,8 +9,10 @@ const postsController = {
         let idPosteo = req.params.id
 
         let detallePosteo = data.posteos.find(posteo => posteo.id == idPosteo)
+        
 
         return res.render('detallePost', {info: detallePosteo})
+        
 
       } ,
     
