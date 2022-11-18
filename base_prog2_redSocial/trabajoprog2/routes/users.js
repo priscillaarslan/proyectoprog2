569af39d0661ac9c37aaa96d66c37af5f8104bf0
priscillaarslan <parslan@udesa.edu.ar>
2022-11-18 12:31:56 -0300

    agregue

--- base_prog2_redSocial/trabajoprog2/routes/users.js
+++ base_prog2_redSocial/trabajoprog2/routes/users.js
@@ -2,11 +2,27 @@ var express = require("express");
 var router = express.Router();
 let usersController = require("../controllers/usersController");
 
+let multer = require('multer');
+let path = require('path');
+
+/* Configurar multer */
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/img/usuarios'));
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+                /* imgPerfil-456456456456456.png  */             
+    }
+});
+
+let upload = multer({storage: storage});
 /* GET users listing. */
 router.get("/login", usersController.Login);
 router.post("/login", usersController.loginPost);
 router.get("/miPerfil", usersController.miPerfil);
 router.get("/registracion", usersController.registracion);
+router.post("/registracion", upload.single('foto'), usersController.registrar);
 router.get("/editarPerfil/:id", usersController.editarPerfil);
 router.get("/logout", usersController.logout);
 
