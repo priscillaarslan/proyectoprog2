859ebef2acdec658e79feb6e992fe7a4e4c31a64
priscillaarslan <parslan@udesa.edu.ar>
2022-11-23 11:46:00 -0300

    agregue

--- base_prog2_redSocial/trabajoprog2/routes/posts.js
+++ base_prog2_redSocial/trabajoprog2/routes/posts.js
@@ -2,9 +2,22 @@ var express = require('express');
 var router = express.Router();
 var postsController = require ('../controllers/postsController')
 
+let multer = require('multer');
+let path = require('path');
 
-/* GET home page. */ /* NOTA: Para que direccion en la url hacemos tal metodo ('/')*/
-router.get('/agregarPost', postsController.agregarPost);
-router.get('/detallePost/:id', postsController.detallePost);
+/*configurar multer*/
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/img/posts'));
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+                /* imgPerfil-456456456456456.png  */             
+    }
+});
+
+let upload = multer({storage: storage});
+
+
 
 module.exports = router;
