5dd2ca392b6de06f5f47cb0894375dbc7acb1334
martinadero <mdaireaux@udesa.edu.ar>
2022-11-23 23:35:28 -0300

    detalles

--- base_prog2_redSocial/trabajoprog2/database/config/config.js
+++ base_prog2_redSocial/trabajoprog2/database/config/config.js
@@ -2,7 +2,7 @@ module.exports={
  "development": {
     "username": "root",
     "password": null,
-    "database": "RedSocial",
+    "database": "redsocial",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3306
